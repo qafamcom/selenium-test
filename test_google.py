12a7807cb2f4a7411dc114c6bd03b402b27c6696
qafamcom <141997900+qafamcom@users.noreply.github.com>
2026-03-09 17:41:54 +0530

    Update test_google.py

--- test_google.py
+++ test_google.py
@@ -1,1 +1,19 @@
-print("GitHub Action working")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Chrome options
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+# Start browser
+driver = webdriver.Chrome(options=options)
+
+# Open Google
+driver.get("https://www.google.com")
+
+print("Google opened successfully")
+
+# Close browser
+driver.quit()
